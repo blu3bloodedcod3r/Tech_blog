6bccdd7bb1dac59e286753944361faaf57cf3cf1
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-17 21:39:14 -0800

    nlogData.json

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,11 @@ const Sequelize = require('sequelize');
 //ternary expression that asks environment for HEROKU(JAWSDB) boiler plate
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
